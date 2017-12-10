a1e984a34ac81fb0cd5d22fbd9ef1f557827aaad
inbalzelinger <inbal.zelinger@gmail.com>
2017-12-10 17:35:42 +0200

    j

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
+
 using namespace std;
 
 #include "Server.h"
 
 int main() {
-    Server s(8005);
+    string port;
+
+    ifstream inFile;
+    /////take of the ../
+    inFile.open("../settings.txt");
+    getline(inFile,port);
+    inFile.close();
+
+
+
+    Server s(atoi(port.c_str()));
     try {
         s.start();
     } catch (const char* msg) {
