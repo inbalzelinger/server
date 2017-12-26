ad41217f053d40224e7a83e77c5d82f7725ebad7
hadarsabag <hadarsbg@gmail.com>
2017-12-26 14:53:37 +0200

    command

--- CommandManager.cpp
+++ CommandManager.cpp
@@ -3,3 +3,15 @@
 //
 
 #include "CommandManager.h"
+CommandManager::CommandManager() {
+}
+void CommandManager::executeCommand(string command, vector<string> args) {
+    Command* commandObj=commandsMap[command];
+    commandObj->execute(args);
+}
+CommandManager::~CommandManager() {
+    map<string,Command*>::iterator it;
+    for(it=commandsMap.begin();it!=commandsMap.end();it++){
+        delete it->second;
+    }
+}
