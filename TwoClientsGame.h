c77196a9c99bd0eaaf20300690ab79d75f181a6c
inbalzelinger <inbal.zelinger@gmail.com>
2018-01-03 18:44:53 +0200

    j

--- TwoClientsGame.h
+++ TwoClientsGame.h
@@ -31,12 +31,55 @@ public:
 */
 
 	TwoClientsGame(string name , int clientOne , int clientTwo);
+
+	/**
+* function name: getName
+* return the name of the game.
+* @return string.
+*/
+
 	string getName();
+	/**
+* function name: setPlayerOne
+* set the client socket of player one.
+* @param clientOne.
+*/
+
 	void setPlayerOne(int clientOne);
+
+	/**
+* function name: setPlayerTwo
+* set the client socket of player two.
+* @param clientTwo.
+*/
 	void setPlayerTwo(int clientTwo);
+
+	/**
+* function name: getClientOne
+* return the socket of player one
+* @return int.
+*/
 	int getClientOne();
+
+	/**
+* function name: getClientTwo
+* return the socket of player two.
+* @return int.
+*/
 	int getClientTwo();
+
+	/**
+* function name: setIsAvailable
+* set the availble field of the game.
+* @param status.
+*/
 	void setIsAvailable(bool status);
+
+	/**
+* function name: getIsAvailable
+* return true if the game is avalble.
+* @return bool.
+*/
 	bool getIsAvailable();
 
 };
